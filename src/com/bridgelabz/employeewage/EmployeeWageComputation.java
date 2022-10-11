e525371e18affd0237a67db54d64d44a6c408185
SachinGadge13 <gadges14@gmail.com>
2022-10-11 20:49:25 +0530

    Added:EmployeeWage For Multiple Companies

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -1,42 +1,41 @@
 package com.bridgelabz.employeewage;
 
-import java.util.Random;
-
 public class EmployeeWageComputation {
+	int isPartTime = 1;
+	int isFullTime = 2;
 
-	static final int isPresent = 1;
-	static final int isPartTime = 2;
+	int computeEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrsInMonth) {
+		// Variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-	private void wagesTillMonth(int salaryPerHour, int totalWorkingDays, int maxWorkingHour, int fullDayWorkingHour) {
-		int empCheck;
-		int empHr = 0;
-		int totalSalary = 0;
-		int day = 0;
-		while (day < totalWorkingDays && (empHr + fullDayWorkingHour) <= maxWorkingHour) {
-			day++;
-			empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-
+		while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
-			case isPresent:
-				empHr += fullDayWorkingHour;
+			case 0:
+				empHrs = 8;
 				break;
-
-			case isPartTime:
-				empHr += (fullDayWorkingHour / 2);
+			case 1:
+				empHrs = 4;
 				break;
-
 			default:
-				empHr += 0;
-				break;
+				empHrs = 0;
 			}
-			System.out.println("Working day : " + day + " (Present : " + empCheck + ")");
+			totalEmpHrs += empHrs;
+			System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		totalSalary = empHr * salaryPerHour;
-		System.out.println("Monthly Employee Wage is : " + totalSalary + "\nTotal working hours are : " + empHr);
+		int totalEmpWage = totalEmpHrs * empRatePerHr;
+		System.out.println("Total Employee Wage for " + company + " is : " + totalEmpWage);
+		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		EmployeeWageComputation wage = new EmployeeWageComputation();
-		wage.wagesTillMonth(20, 20, 100, 8);
+		EmployeeWageComputation Result = new EmployeeWageComputation();
+		System.out.println("Welcome to the Employee Wage Computation Calculation");
+
+		Result.computeEmpWage("Flipkart", 20, 20, 100); // static int
+		Result.computeEmpWage("Google", 25, 22, 100);
+		Result.computeEmpWage("Microsoft", 25, 18, 100);// --empRatePerHr--numOfWorkingDays--maxHrsInMonth
+
 	}
 }
